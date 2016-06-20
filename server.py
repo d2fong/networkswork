c116292b364cee245bf0621d11512af9a5cb8095
d2fong <d2fong@uwaterloo.ca>
2016-06-20 15:14:38 -0400

    Starts server implementation - Adds error code text file to document the meaning of error codes - Starts the server implementation functions such as the spinup and serve functions - Adds default server params for testing configurations - Adds a few helper functions for logging and errors

--- server.py
+++ server.py
@@ -1,25 +1,70 @@
+import socket
 
+# ERROR CODES
+SERVER_ALREADY_STARTED = 5
+
+
+
+
+def error(code, msg):
+	print "[ERROR]: %s, %d"%(msg, code)
+
+def log(lvl, msg):
+	print "[%s]: %s"%(lvl, msg)
+
+DEFAULT_SERVER_PORT = 8899
+DEFAULT_MAX_CONNECTIONS = 5
+DEFAULT_HOME_DIR = './home_server'
 
 
 
 class Server:
 
-	def __init__(self, addr, port, homeDir):
+	def __init__(self, addr=None,
+	 port=DEFAULT_SERVER_PORT,
+	 homeDir=DEFAULT_HOME_DIR,
+	 maxConnections=DEFAULT_MAX_CONNECTIONS):
 		self.addr = addr
 		self.port = port
 		self.homeDir = {
 			"path": homeDir,
 			"fileList": []
 		}
-
+		self.socket = None
+		self.maxConnections = maxConnections
 
 	# wonky things for storing the files
-	def setup_homeDir(self):
+	# given a home directory, for each file in the
+	# directory, add it to the file list
+	# 
+	def setup_homeDirAndFileList(self):
+		# for f in self.homeDir:
+			# self.fileList.append(f)
 		pass
 
 	# setup the socket stuff, connect etc..
 	def spinup(self):
-		pass
+		if self.socket is not None:
+			error(5, "The server has already been started")
+		else:
+			self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+			if self.addr is None:
+				self.addr = socket.gethostname()
+
+			self.socket.bind((self.addr , self.port))
+			log("INFO", "addr: %s, port: %d"%(self.addr, self.port))
+			self.waitAndServe()
+
+	def waitAndServe(self):
+		while True:
+			data, address = self.sock.recvfrom(4096)
+
+			log("INFO", "Received %s bytes from address %s"% (len(data), address))
+			log("INFO", "Message contents: %s", data)
+
+
+
 
 	def create_connection(self):
 		pass
@@ -72,5 +117,7 @@ def send_file(fname):
 
 
 def serverTest():
-	x = Server("", 8080, ".")
+	x = Server(port=8080, homeDir=" ")
+	x.spinup()
 
+serverTest()
